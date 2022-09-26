9828628e911da7264d2d6d10ef9cc4e27a373fcd
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-26 17:14:02 -0400

    the date props needed the .totoISOString() to work

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2022, 8, 23);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 200;
 
   return (
     <div className='expense-item'>
-      <div>{props.date}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
         <h2 className='expense-item h2'>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
