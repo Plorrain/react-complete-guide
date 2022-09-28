334c12c6286fadce9a11316f9abdfbc59ae695c3
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-28 16:33:21 -0400

    new course module. Slowly getting to state and adding a button to the app with a example of a state function

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -3,13 +3,16 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
+const clickHandler = () => {
+  console.log('clicked!')
+};
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
           <h2 className='expense-item h2'>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
+          <button onClick={clickHandler}>Change Title</button>
         </div>
     </Card>
   );
