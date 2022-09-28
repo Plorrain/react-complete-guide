1ae7071d646dd582279af02313b261a891bca5ea
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-28 16:43:07 -0400

    importing useState to the expenseItem file

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 const clickHandler = () => {
-  console.log('clicked!')
+  title = 'Updated!';
 };
+
+let title = props.title;
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-          <h2 className='expense-item h2'>{props.title}</h2>
+          <h2 className='expense-item h2'>{title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
           <button onClick={clickHandler}>Change Title</button>
         </div>
