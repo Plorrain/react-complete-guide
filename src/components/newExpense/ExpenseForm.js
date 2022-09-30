43cc81f9b4a59456d167fe95f1ffd922cb855b7e
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-30 15:32:44 -0400

    applying same logic to date and amount states

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm() {
+  const [ enteredTitle, setEnteredTitle ] = useState('');
+  const [ enteredAmount, setEnteredAmount ] = useState('');
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value)
+    setEnteredTitle(event.target.value);
   };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {};
 
   return (
       <form>
@@ -15,11 +23,15 @@ function ExpenseForm() {
           </div>
           <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' min='0.01' step='0.01' />
+            <input type='number' min='0.01' step='0.01'
+            onChange={amountChangeHandler}
+            />
           </div>
           <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min='2022-01-01' max='2024-01-01' />
+            <input type='date' min='2022-01-01' max='2024-01-01'
+            onChange={dateChangeHandler}
+            />
           </div>
         </div>
         <div className='new-expense__actions'>
