13b17cbe7135535714f52afdcfecf8412e30d2fd
Philippine Lorrain <philippinelorrain@gmail.com>
2022-09-30 11:01:51 -0400

    setting the NewExpense file + adding a NewExpense.css file

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './NexExpense.css';
 
 function NewExpense() {
-
+return (
+    <div className='new-expense'>
+      <form></form>
+    </div>
+  )
 };
 
 export default NewExpense;
