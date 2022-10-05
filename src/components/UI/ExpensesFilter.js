50d5a860f3544c1b72abc238e1a7b795aa065356
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-05 15:08:24 -0400

    completing challenge:added onChange attribute to select tag + created pickedYearHandler. Will create a props for pickedYear state to link in other file

--- src/components/UI/ExpensesFilter.js
+++ src/components/UI/ExpensesFilter.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { UseState } from 'react';
 
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  const [pickedyear, setPickedYear] = UseState('');
+  const pickYearHandler = (event) => {
+    setPickedYear(event.target.value);
+  };
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label className='expenses-filter label'>
           Filter by year
         </label>
-        <select className='expenses-filter select'>
+        <select className='expenses-filter select'
+        onChange={pickYearHandler}
+        >
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
