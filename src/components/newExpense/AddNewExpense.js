d5b0ce5bdccc764bff94d212909e9e97f90cef33
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-11 09:44:55 -0400

    challenge for installing an addnewexpense button instead of the form when unclicked

---
+++ src/components/newExpense/AddNewExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewExpense.css'
+
+function AddNewExpense() {
+
+  return (
+  <div className='new-expense'>
+    <button className='new-expense button'>Add New Expense</button>
+  </div>
+  )
+}
+export default AddNewExpense;
