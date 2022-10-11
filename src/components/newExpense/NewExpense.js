05f1e5e84bce3537be58d0b463411d80372c213c
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-11 10:04:13 -0400

    everything is being done from Newexpense.js

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 function NewExpense(props) {
   // function below helps us pass props from expenseform(child)
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,8 +14,13 @@ function NewExpense(props) {
     props.onAddExpense(expenseData);
   };
 
+  const startEditingHandler = () => {
+     setIsEditing(true)
+  };
+
 return (
     <div className='new-expense'>
+      <button onClick={startEditingHandler}>Add New Expense</button>
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   )
