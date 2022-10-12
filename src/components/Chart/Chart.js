f9ad46b9e275af10ab32341a97ab207a7a77a1d2
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-12 15:30:29 -0400

    putting value, label and key as attributes of chartbar component-->next implementing chartbar component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -7,12 +7,16 @@ function Chart(props) {
   return (
     <div className='chart'>
       {/* we could render the chartBar component 12x or see below */}
-      {props.dataPoints.map(dataPoint =>
-        <ChartBar value={dataPoint.value}
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
         />
-      )}
+      ))}
     </div>
-  )
+  );
 };
 
 export default Chart;
