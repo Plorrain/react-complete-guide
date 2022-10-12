9680d405ecb97be80d9c8637ef56507760aaa04f
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-12 16:10:12 -0400

    finishing the expense tracker

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,8 +3,8 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 function Chart(props) {
-  const dataPointsValues = props.dataPoints.map();
-  const totalMaximum = Math.max();
+  const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointsValues);
 
   return (
     <div className='chart'>
@@ -13,7 +13,7 @@ function Chart(props) {
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
